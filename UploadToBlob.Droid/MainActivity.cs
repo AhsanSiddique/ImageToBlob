36499e215ef4e703850a92ec19a323e916dfdf2d
Ahsan Siddique <ahsan.siddiq@outlook.com>
2018-12-16 20:37:57 +0500

    Last Changes

--- UploadToBlob.Droid/MainActivity.cs
+++ UploadToBlob.Droid/MainActivity.cs
@@ -15,17 +15,21 @@ namespace UploadToBlob.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        private Button btnUpload, btnChoose , btnCapture;
+        private Button btnUpload, btnChoose, btnCapture;
         private ImageView imgView;
-        private EditText edtURL;
+        public Bitmap mBitMap;
         private Android.Net.Uri filePath;
         private const int PICK_IMAGE_REQUSET = 71;
-
+        private const int TAKE_IMAGE_REQUSET = 0;
+        private EditText edtURL;
+        private MemoryStream inputStream;
+        private ProgressBar progressBar;
         public string URL { get; private set; }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
             btnChoose = FindViewById<Button>(Resource.Id.btnChoose);
@@ -33,18 +37,41 @@ namespace UploadToBlob.Droid
             btnCapture = FindViewById<Button>(Resource.Id.btnCapture);
             imgView = FindViewById<ImageView>(Resource.Id.imgView);
             edtURL = FindViewById<EditText>(Resource.Id.edtURL);
-            //Events  
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+
+            //Events
             btnChoose.Click += delegate
             {
                 ChooseImage();
             };
-            btnUpload.Click += delegate 
-            {
 
+            btnCapture.Click += delegate
+            {
+                CaptureImage();
+            };
+
+            btnUpload.Click += delegate
+            {
+                UploadImage();
+                Busy();
             };
         }
+
+        private void CaptureImage()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+        }
+
+        private void UploadImage()
+        {
+            if (inputStream != null)
+                Upload(inputStream);
+        }
+
         private void ChooseImage()
         {
+
             Intent intent = new Intent();
             intent.SetType("image/*");
             intent.SetAction(Intent.ActionGetContent);
@@ -53,6 +80,7 @@ namespace UploadToBlob.Droid
         protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
+
             if (requestCode == PICK_IMAGE_REQUSET &&
                 resultCode == Result.Ok &&
                 data != null &&
@@ -61,8 +89,36 @@ namespace UploadToBlob.Droid
                 filePath = data.Data;
                 try
                 {
-                    Bitmap bitmap = MediaStore.Images.Media.GetBitmap(ContentResolver, filePath);
-                    imgView.SetImageBitmap(bitmap);
+                    mBitMap = MediaStore.Images.Media.GetBitmap(ContentResolver, filePath);
+                    imgView.SetImageBitmap(mBitMap);
+                    byte[] bitmapData;
+                    using (var stream = new MemoryStream())
+                    {
+                        mBitMap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
+                        bitmapData = stream.ToArray();
+                    }
+                    inputStream = new MemoryStream(bitmapData);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else if (requestCode == 0 &&
+                resultCode == Result.Ok &&
+                data != null)
+            {
+                try
+                {
+                    mBitMap = (Bitmap)data.Extras.Get("data");
+                    imgView.SetImageBitmap(mBitMap);
+                    byte[] bitmapData;
+                    using (var stream = new MemoryStream())
+                    {
+                        mBitMap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
+                        bitmapData = stream.ToArray();
+                    }
+                    inputStream = new MemoryStream(bitmapData);
                 }
                 catch (IOException ex)
                 {
@@ -71,18 +127,42 @@ namespace UploadToBlob.Droid
             }
         }
 
+        //Upload to blob function
         private async void Upload(Stream stream)
         {
-            var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=blobacount;AccountKey=hMZuDKfdz1iGPVsV+W9V52YnsjD6F1tjdH89XIw0QM3J6FB+tdJ9IgQI+OAWHgCRKSYMK0EwGcpB0qCJI8kY+w==;EndpointSuffix=core.windows.net");
-            var client = account.CreateCloudBlobClient();
-            var container = client.GetContainerReference("images");
-            await container.CreateIfNotExistsAsync();
-            var name = Guid.NewGuid().ToString();
-            var blockBlob = container.GetBlockBlobReference($"{name}.png");
-            await blockBlob.UploadFromStreamAsync(stream);
-            URL = blockBlob.Uri.OriginalString;
-            edtURL.Text = URL;
-            Toast.MakeText(this, "Image uploaded to Blob Storage Successfully", ToastLength.Short).Show();
+            try
+            {
+                var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=blobacount;AccountKey=hMZuDKfdz1iGPVsV+W9V52YnsjD6F1tjdH89XIw0QM3J6FB+tdJ9IgQI+OAWHgCRKSYMK0EwGcpB0qCJI8kY+w==;EndpointSuffix=core.windows.net");
+                var client = account.CreateCloudBlobClient();
+                var container = client.GetContainerReference("images");
+                await container.CreateIfNotExistsAsync();
+                var name = Guid.NewGuid().ToString();
+                var blockBlob = container.GetBlockBlobReference($"{name}.png");
+                await blockBlob.UploadFromStreamAsync(stream);
+                URL = blockBlob.Uri.OriginalString;
+                edtURL.Text = URL;
+                Toast.MakeText(this, "Image uploaded to Blob Storage Successfully!", ToastLength.Short).Show();
+                NotBusy();
+            }
+            catch (Exception e)
+            {
+                Toast.MakeText(this, "" + e.ToString(), ToastLength.Short);
+            }
+        }
+
+        void Busy()
+        {
+            btnCapture.Enabled = false;
+            btnChoose.Enabled = false;
+            btnUpload.Enabled = false;
+            progressBar.Visibility = Android.Views.ViewStates.Visible;
+        }
+        void NotBusy()
+        {
+            btnCapture.Enabled = true;
+            btnChoose.Enabled = true;
+            btnUpload.Enabled = true;
+            progressBar.Visibility = Android.Views.ViewStates.Invisible;
         }
     }
 }
